7433ecd85dfff52558ac6d389b99d0c26b89def2
dududedaxiong <764840990@qq.com>
2026-01-24 13:32:33 +0000

    �� Auto sync ad blocking rules

--- js-ad-rules.js
+++ js-ad-rules.js
@@ -57,7 +57,7 @@
     },
     "/cn\\d+\\.91short\\.com/": {
         "blockUrls": [
-            "*/public/ad*\.js?v=*",
+            "*/public/ad**v=*",
             "https://**.icu/**?**",
             "https://**/public/axios.min.js",
             "https://**/worker/PicWorker.js?v=**",
