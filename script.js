7ae91ea22750de4bc1da1c28130b735ec467bcb8
dududedaxiong <764840990@qq.com>
2026-01-27 14:15:16 +0000

    up

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
 (() => {
-    const global = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : this;
-    var content = global.YYKM.fetch("https://perfecttv.net/PerfecttvFree.m3u#sp;ua=okhttp");
-    const lines = content.split('\n');
-    return lines.map(line => {
-        let trimLine = line.trim();
-        if (!trimLine || trimLine.startsWith("#EXTM3U")) return line;
-        if (trimLine.startsWith("#EXTINF:")) {
-            let firstCommaIndex = line.indexOf(",");
-            if (firstCommaIndex !== -1) {
-                let prefix = line.substring(0, firstCommaIndex + 1); 
-                let target = line.substring(firstCommaIndex + 1);
-                target = target.replaceAll("12", "34");
-                
-                return prefix + target;
-            }
-        }
-        if ((trimLine.includes(",") && !trimLine.startsWith("#")) || /^(http|https|rtp|p3p|rtsp|mitv|video|webview):\/\//i.test(trimLine)) {
-            let target = line;
-            target = target.replaceAll("12", "34");
-                
-            return target;
-        }
-        return line;
+    const global = globalThis;
+    var content = global.YYKM.fetch("https://perfecttv.net/PerfecttvFree.m3u");
+    return content.split('\n').map(line => {
+        let target = line;
+        target = target.replaceAll("video://", "webview://");
+            
+        return target;
     }).join('\n');
 })();
