a7259b708fd71336842bb937482e088829437b69
dududedaxiong <764840990@qq.com>
2026-01-27 08:41:43 +0000

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 (() => {
   const global = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : this;
-  // 1. 获取数据（内嵌 UA）
-  var content = global.YYKM.fetch("https://img.131213.xyz/tfile/BQACAgUAAx0Eflp52gABASORaXdbpO0p7IMpTiebDJy6u4WwxewAAq4aAAIKH7lXS88ZiCssf4Q4BA#sp;ua=okhttp");
-  // 2. 替换内容
+  // 1. 获取订阅源数据（已注入 UA 和 Hash 参数）
+  var content = global.YYKM.fetch("https://img.131213.xyz/tfile/BQACAgUAAx0Eflp52gABASORaXdbpO0p7IMpTiebDJy6u4WwxewAAq4aAAIKH7lXS88ZiCssf4Q4BA#sp;hash=time-interval://6h;ua=okhttp");
+  // 2. 执行内容替换
   content = content
     .replaceAll("https://", "webview://https://");
   return content;
