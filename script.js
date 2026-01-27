c821d22a227e718cb87803e136af5388cee4ad88
dududedaxiong <764840990@qq.com>
2026-01-27 09:48:08 +0000

    up

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (() => {
     const g = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : this;
-    var c = g.YYKM.fetch("https://live.ottiptv.cc/iptv.m3u?userid=5464346556&sign=25ea864f3e810344b62f867fc74d45773b80a171b11777b0015a2604734c4fd9a59f038acd4ac5c7caa8c06eb5a66cd8a60cdd66b4c87fa187c518e40d7bf5eccf1a164a2e58c8&auth_token=0502282be476cd93610594ed29cb967c#sp;ua=okhttp");
+    var c = g.YYKM.fetch("https://live.ottiptv.cc/iptv.m3u?userid=5464346556&sign=25ea864f3e810344b62f867fc74d45773b80a171b11777b0015a2604734c4fd9a59f038acd4ac5c7caa8c06eb5a66cd8a60cdd66b4c87fa187c518e40d7bf5eccf1a164a2e58c8&auth_token=0502282be476cd93610594ed29cb967c#sp;ua=okHttp/Mod-1.5.0.0");
     return c.split('\n').map(l => {
         let t = l.trim();
         if (!t || t.startsWith("#EXTM3U")) return l;
