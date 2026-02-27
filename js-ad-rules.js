13911f2ade815f50781b94de8a9fd59900dde8bf
dududedaxiong <764840990@qq.com>
2026-02-27 16:34:50 +0000

    �� Auto sync ad blocking rules

--- js-ad-rules.js
+++ js-ad-rules.js
@@ -268,7 +268,7 @@
             ".van-tab.i(-3,-2)",
             "li.navbar-item.i(-3,-2)",
             "a.abk-item.abk-banner",
-            "div[class*='left']"
+            "id[class*='left']"
             
         ],
         "idsToHide": [
