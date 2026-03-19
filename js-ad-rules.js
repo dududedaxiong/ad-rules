9b8ff03cfc5b03415cc32f47541229e6ddb84a5d
dududedaxiong <764840990@qq.com>
2026-03-19 16:15:43 +0000

    �� Auto sync ad blocking rules

--- js-ad-rules.js
+++ js-ad-rules.js
@@ -63,7 +63,7 @@
             "uni-view.tabbar-list",
             "uni-view[style=\"display: flex; flex-wrap: wrap; justify-content: space-between;\"]",
             "a[class^='downurl']",
-           "div[style*=\"position:fixed; bottom\"]"
+           "div[style*='opacity:0.01']"
         ],
         "idsToHide": [
             "__clb-spot_2090934_fxi_1",
